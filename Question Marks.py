b0749c6e03a3865c0265d0d085460ef9895af5ac
Astro-Ed <55730689+Astro-Ed@users.noreply.github.com>
2019-09-23 22:42:04 -0700

    Add files via upload

---
+++ Question Marks.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 21:06:17 2019
+
+@author: Astro.Ed
+"""                
+
+def QuestionsMarks(str):
+    sum_digit = 0
+    for x in str: # creating a loop to check each char in the string
+        if x.isdigit() == True:
+            z = int(x)
+            sum_digit = sum_digit + z
+            if sum_digit == 10:
+                break     
+    if "???" in str:
+        print('True')
+    else:
+        print('False')
+    return str
+# keep this function call here  
+QuestionsMarks(input()) 
